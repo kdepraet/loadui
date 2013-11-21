d9a6e1c393c729a91c91f815a470fe811a83a73e
Renato Athaydes <renato.deathaydes@smartbear.com>
2013-11-21 14:57:15 +0100

    LOADUI-1369 Fixing and refactoring tests

--- loadui-fx-integration-tests/src/main/java/com/eviware/loadui/test/ui/fx/FxIntegrationBase.java
+++ loadui-fx-integration-tests/src/main/java/com/eviware/loadui/test/ui/fx/FxIntegrationBase.java
@@ -1,6 +1,7 @@
 package com.eviware.loadui.test.ui.fx;
 
 import com.eviware.loadui.api.model.ProjectItem;
+import com.eviware.loadui.api.model.WorkspaceItem;
 import com.eviware.loadui.api.model.WorkspaceProvider;
 import com.eviware.loadui.ui.fx.util.test.ComponentHandle;
 import com.eviware.loadui.ui.fx.util.test.LoadUiRobot;
@@ -167,7 +168,7 @@ public class FxIntegrationBase extends GuiTest
 		System.out.println( "Component node: " + componentNode );
 		Set<Node> knobs = findAll( "#knob", componentNode );
 
-		Node knob = Lists.newArrayList(knobs).get( number - 1 );
+		Node knob = Lists.newArrayList( knobs ).get( number - 1 );
 
 		return new KnobHandle( knob );
 	}
@@ -242,8 +243,12 @@ public class FxIntegrationBase extends GuiTest
 
 	public static ProjectItem getProjectItem()
 	{
-		return BeanInjector.getBean( WorkspaceProvider.class ).getWorkspace()
-				.getProjects().iterator().next();
+		return getWorkspaceItem().getProjects().iterator().next();
+	}
+
+	public static WorkspaceItem getWorkspaceItem()
+	{
+		return BeanInjector.getBean( WorkspaceProvider.class ).getWorkspace();
 	}
 
 	public class KnobHandle
@@ -267,4 +272,25 @@ public class FxIntegrationBase extends GuiTest
 			return this;
 		}
 	}
+<<<<<<< Updated upstream
+=======
+
+	public void waitAndClick( final String query )
+	{
+		TestUtils.awaitConditionSilent( new Callable<Boolean>()
+		{
+			@Override
+			public Boolean call() throws Exception
+			{
+				return exists( query );
+			}
+		}, 5 );
+
+		if( !exists( query ) )
+		{
+			fail( "Expected to find match for query \"" + query + "\" within wait period but failed. Captured screen shot: " + captureScreenshot().getAbsolutePath() );
+		}
+		click( query );
+	}
+>>>>>>> Stashed changes
 }
